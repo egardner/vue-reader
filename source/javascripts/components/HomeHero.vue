5e5af22f4ac62b4bada47c3c407662ade040f6ea
Eric Gardner <gardner.ec@gmail.com>
2016-01-14 11:18:49 -0800

    Add repo counter to button
    This is a basic example of an idiomatic ES6 ajax call. Native
    XMLHttpRequests seem to work just fine for now; vue-resource may not be
    necessary. Note the syntax for methods and the use of fat-arrow binding
    of `this`. Javascript is actually becoming somewhat pleasant to write in
    with these new rules.

--- source/javascripts/components/HomeHero.vue
+++ source/javascripts/components/HomeHero.vue
@@ -8,8 +8,31 @@
       h1 Welcome to the Infinite Library
       p.
         Discover thousands of titles in Project Gutenberg!
+      button Search {{ repoCount }} Books!
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      repoCount: ''
+    }
+  },
+  methods: {
+    getRepoCount() {
+      let userID = 'GITenberg'
+      let url    = 'https://api.github.com/users/' + userID
+      let xhr    = new XMLHttpRequest()
+      xhr.open('GET', url)
+      xhr.onload = () => {
+        this.$set('repoCount', JSON.parse(xhr.responseText).public_repos)
+        console.log(JSON.parse(xhr.responseText))
+      }
+      xhr.send()
+    }
+  },
+  ready() {
+    this.getRepoCount()
+  }
+}
 </script>
